e626f1f04183287e56cf16af67713de78e89a8d7
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-12-11 18:03:11 +0530

    few changes

--- whether_app/views.py
+++ whether_app/views.py
@@ -6,6 +6,18 @@ def home(request):
     url = "https://api.openweathermap.org/data/2.5/weather?q={},&appid={}"
     city = "mumbai"
     api_key = "43055cdc7ce9f10e843e86624caa9061"
-    resp = requests.get(url.format(city, api_key))
-    print(resp.text)
-    return render(request, 'index.html')
+    resp = requests.get(url.format(city, api_key)).json()
+
+    city_weather = {
+        'city': city.capitalize(),
+        'temperature': resp['main']['temp'],
+        'description': resp['weather'][0]['description'].capitalize(),
+        'icon': resp['weather'][0]['icon']
+    }
+
+    result = {
+        'city_weather': city_weather
+    }
+
+    print(city_weather)
+    return render(request, 'index.html', result)
