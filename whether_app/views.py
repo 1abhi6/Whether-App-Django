6ab91d788738c65840c93270963eda625db41714
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-12-11 14:28:56 +0530

    API Setup

--- whether_app/views.py
+++ whether_app/views.py
@@ -3,7 +3,10 @@ import requests
 
 
 def home(request):
-    url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid=43055cdc7ce9f10e843e86624caa9061"
-    resp = requests.get(url)
-    print(resp.status_code)
+    url = "https://api.openweathermap.org/data/2.5/weather?q={},&appid={}"
+    # url = "https://api.openweathermap.org/data/2.5/weather?q={},{}&callback=test&appid={}"
+    city = "mumbai"
+    api_key = "43055cdc7ce9f10e843e86624caa9061"
+    resp = requests.get(url.format(city, api_key))
+    print(resp.text)
     return render(request, 'index.html')
