5bc5e1802415ebfb7a857b93f4c646132c4ce319
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-12-11 18:35:25 +0530

    handle input

--- whether_app/views.py
+++ whether_app/views.py
@@ -3,21 +3,30 @@ import requests
 
 
 def home(request):
-    url = "https://api.openweathermap.org/data/2.5/weather?q={},&appid={}"
-    city = "mumbai"
-    api_key = "43055cdc7ce9f10e843e86624caa9061"
-    resp = requests.get(url.format(city, api_key)).json()
+    result = {}
+    city = ""
+    query = ""
+    if request.method == 'POST':
+        try:
 
-    city_weather = {
-        'city': city.capitalize(),
-        'temperature': round(resp['main']['temp']-273.15,2),
-        'description': resp['weather'][0]['description'].capitalize(),
-        'icon': resp['weather'][0]['icon']
-    }
+            url = "https://api.openweathermap.org/data/2.5/weather?q={},&appid={}"
+            query = request.POST.get('queryBox')
+            city = query.lower()
+            api_key = "43055cdc7ce9f10e843e86624caa9061"
+            resp = requests.get(url.format(city, api_key)).json()
 
-    result = {
-        'city_weather': city_weather
-    }
+            city_weather = {
+                'city': city.capitalize(),
+                'temperature': round(resp['main']['temp']-273.15, 2),
+                'description': resp['weather'][0]['description'].capitalize(),
+                'icon': resp['weather'][0]['icon']
+            }
 
-    print(city_weather)
+            result = {
+                'city_weather': city_weather,
+                'city_name': query
+            }
+        except:
+            pass
+
     return render(request, 'index.html', result)
