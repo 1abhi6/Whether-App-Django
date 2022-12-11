f12010db03ddb2b7b75c3ea2e3de2800b31a58c4
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-12-11 12:15:38 +0530

    api setup

--- whether_app/views.py
+++ whether_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+import requests
 
 
 def home(request):
-    return render(request,'index.html')
-    
+    url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid=43055cdc7ce9f10e843e86624caa9061"
+    resp = requests.get(url)
+    print(resp.status_code)
+    return render(request, 'index.html')
